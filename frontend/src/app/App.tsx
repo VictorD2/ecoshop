55d48d19eaf15ce9bc1fba7e705a0315fbbb180f
Joseph Rivas <razorij20@gmail.com>
2021-10-12 20:01:48 -0500

    Pagina Registrar

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -7,6 +7,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import NotFound from "../pages/NotFound";
 import DashBoard from "../pages/Dashboard/DashBoard";
+import Carrito from '../pages/Carrito/Carrito';
 
 // Layout
 import LayoutDash from "../partials/LayoutDash";
@@ -19,12 +20,12 @@ const App = () => {
       <Switch>
         {/* Vistas */}
         <LayoutUsuario exact path="/" component={Home} />
+        <LayoutUsuario exact path="/Carrito" component={Carrito} />
         <Route exact component={Login} path="/Iniciar" />
-        <Route exact component={Register} path="/Registrarse" />
+        <Route exact component={Register} path="/Register" />
 
         {/* Dashboard */}
         <LayoutDash exact path="/DashBoard" component={DashBoard} />
-
 
         <Route component={NotFound} />
       </Switch>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,313 +1,63 @@
-import React, { ChangeEvent, FormEvent, useState, useRef, RefObject, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
-import swal from "sweetalert";
-import auth from "../auth/auth";
-import { API } from "../config/config";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../images/logo.png';
 
-// import logoRegister from "../images/Logo.svg";
-
-//Toast
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "animate.css/animate.min.css";
-
-import { useUsuario } from "../auth/UsuarioProvider";
-import exprRegular from "../helpers/encrypt/regularExpr";
-
-// Interfaces
-interface Usuario {
-  name: string;
-  surname: string;
-  email: string;
-  password: string;
-  verifyPassword: string;
-  rut: string;
-  telefono: string;
-  id_pais_nacimiento: string;
-  id_pais_residencia: string;
-  profesion: string;
-}
-interface Pais {
-  id_pais: string;
-  nombre_pais: string;
-  url_foto_pais: string;
-}
-const Register: React.FC = () => {
-  //Initial State
-  const history = useHistory();
-  const { setUsuario } = useUsuario();
-  const [usuarioR, setUsuarioR] = useState<Usuario>({
-    name: "",
-    surname: "",
-    email: "",
-    rut: "",
-    telefono: "",
-    id_pais_nacimiento: "AF",
-    id_pais_residencia: "AF",
-    password: "",
-    profesion: "",
-    verifyPassword: "",
-  });
-
-  // CAPTCHA State
-
-  // REFERENCE
-
-  const parrafoName = useRef<HTMLParagraphElement>(null);
-  const parrafoSurname = useRef<HTMLParagraphElement>(null);
-  const parrafoEmail = useRef<HTMLInputElement>(null);
-  const parrafoProfesion = useRef<HTMLParagraphElement>(null);
-  const parrafoTelephone = useRef<HTMLParagraphElement>(null);
-  const parrafoRut = useRef<HTMLParagraphElement>(null);
-
-  const refPasswordVerify = useRef<HTMLInputElement>(null);
-  const refPassword = useRef<HTMLInputElement>(null);
-  const [paises, setPaises] = useState<Pais[]>([]);
-  const [paisNaci, setPaisNaci] = useState<Pais>({
-    nombre_pais: "Afganistan",
-    id_pais: "AF",
-    url_foto_pais: "/uploads/paises/afganistan.png",
-  });
-  const [paisResi, setPaisResi] = useState<Pais>({
-    nombre_pais: "Afganistan",
-    id_pais: "AF",
-    url_foto_pais: "/uploads/paises/afganistan.png",
-  });
-
-  useEffect(() => {
-    window.scrollTo({ top: 0 });
-    getPaises();
-    return () => {};
-  }, []);
-
-  const getPaises = async () => {
-    const res = await axios.get(`${API}/api/v0/pais`);
-    if (res.data.error) return;
-    setPaises(res.data.paises);
-  };
-
-  // Validación de campos
-  const validation = (expr: RegExp, input: EventTarget & (HTMLInputElement | HTMLSelectElement), msg: RefObject<HTMLParagraphElement>) => {
-    if (expr.test(input.value)) {
-      input.classList.remove("is-invalid");
-      msg.current?.classList.add("d-none");
-      return;
-    }
-    input.classList.add("is-invalid");
-    msg.current?.classList.remove("d-none");
-  };
-
-  const validationPassword = () => {
-    if (refPassword.current?.value === refPasswordVerify.current?.value) {
-      refPassword.current?.classList.remove("is-invalid");
-      refPasswordVerify.current?.classList.remove("is-invalid");
-      return;
-    }
-    refPassword.current?.classList.add("is-invalid");
-    refPasswordVerify.current?.classList.add("is-invalid");
-  };
-
-  // Change Event
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    setUsuarioR({ ...usuarioR, [e.target.name]: e.target.value });
-    switch (e.target.name) {
-      case "name":
-        validation(exprRegular.nombre, e.target, parrafoName);
-        break;
-      case "surname":
-        validation(exprRegular.nombre, e.target, parrafoSurname);
-        break;
-      case "email":
-        validation(exprRegular.correo, e.target, parrafoEmail);
-        break;
-      case "profesion":
-        validation(exprRegular.nombre, e.target, parrafoProfesion);
-        break;
-      case "telefono":
-        validation(exprRegular.telefono, e.target, parrafoTelephone);
-        break;
-      case "rut":
-        validation(exprRegular.rut, e.target, parrafoRut);
-        break;
-      case "password":
-        validationPassword();
-        break;
-      case "verifyPassword":
-        validationPassword();
-        break;
-      case "id_pais_nacimiento":
-        for (let i = 0; i < paises.length; i++) {
-          const pais = paises[i];
-          if (pais.id_pais === e.target.value) {
-            setPaisNaci({ ...paisNaci, url_foto_pais: pais.url_foto_pais });
-            return;
-          }
-        }
-        break;
-      case "id_pais_residencia":
-        for (let i = 0; i < paises.length; i++) {
-          const pais = paises[i];
-          if (pais.id_pais === e.target.value) {
-            setPaisResi({ ...paisResi, url_foto_pais: pais.url_foto_pais });
-            return;
-          }
-        }
-        break;
-    }
-  };
-
-  const enviarDatos = async (user: any, url_foto_residencia: string, url_foto_nacimiento: string) => {
-    user.url_foto_residencia = url_foto_residencia;
-    user.url_foto_nacimiento = url_foto_nacimiento;
-    return await axios.post(`${API}/signup`, user);
-  };
-
-  //Submit
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (exprRegular.nombre.test(usuarioR.name) && exprRegular.nombre.test(usuarioR.surname) && usuarioR.password === usuarioR.verifyPassword && exprRegular.correo.test(usuarioR.email) && exprRegular.nombre.test(usuarioR.profesion) && exprRegular.telefono.test(usuarioR.telefono) && usuarioR.id_pais_nacimiento && usuarioR.id_pais_residencia && exprRegular.rut.test(usuarioR.rut)) {
-      const datos = await enviarDatos(usuarioR, paisResi.url_foto_pais, paisNaci.url_foto_pais);
-      if (datos.data.success) {
-        setUsuario(datos.data.user);
-        auth.setRango(datos.data.user.id_rango);
-        auth.sigIn();
-        history.push("/");
-      }
-      if (datos.data.error) return swal("Oops!", datos.data.error, "error");
-      return;
-    }
-    return swal("Oops!", "Campos invalidos", "error");
-  };
-
+function Register() {
   return (
-    <>
-      <div className="rgt__main">
-        <ToastContainer />
-        <div className="card content__form animate__animated animate__flipInY my-auto">
-          <Link to="/" className="card-header rgt__header">
-            {/* <img className="rgt__img" src={logoRegister} alt="logo-register" /> */}
-            <h5 className="rgt__title">FAMIR CENTRO</h5>
+    <div className="content-main">
+      <div className="container px-5">
+        <div className="d-flex justify-content-center my-5">
+          <Link to="/">
+            <img src={Logo} alt="logo-register" className="mx-auto my-auto logo-register" />
           </Link>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-12">
-                <form className="needs-validation" noValidate onSubmit={handleSubmit}>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label">Nombres</label>
-                        <input value={usuarioR.name} onChange={handleInputChange} className="form-control rgt__form-control" type="text" name="name" />
-                        <p className="text-danger fw-light d-none mt-2" ref={parrafoName} style={{ fontSize: "0.75rem" }}>
-                          El nombre solo puede contener letras y espacios.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label">Apellidos</label>
-                        <input value={usuarioR.surname} onChange={handleInputChange} className="form-control rgt__form-control" type="text" name="surname" />
-                        <p className="text-danger fw-light d-none mt-2" ref={parrafoSurname} style={{ fontSize: "0.75rem" }}>
-                          El apellido solo puede contener letras y espacios.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label">Correo</label>
-                        <input value={usuarioR.email} onChange={handleInputChange} className="form-control rgt__form-control" type="email" name="email" placeholder="name@example.com" />
-                        <p className="text-danger fw-light d-none mt-2" ref={parrafoEmail} style={{ fontSize: "0.75rem" }}>
-                          El email debe tener formato name@example.com
-                        </p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label">Profesión</label>
-                        <input value={usuarioR.profesion} onChange={handleInputChange} className="form-control rgt__form-control" type="text" name="profesion" />
-                        <p className="text-danger fw-light d-none mt-2" ref={parrafoProfesion} style={{ fontSize: "0.75rem" }}>
-                          La profesion debe contener letras y espacios
-                        </p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="input-group mb-3">
-                        <p className="w-100 mb-0">Pais de Nacimiento</p>
-                        <label className="input-group-text" htmlFor="inputGroupSelect01">
-                          <img src={paisNaci.url_foto_pais} className="img__pais register" alt="" />
-                        </label>
-                        <select value={usuarioR.id_pais_nacimiento} onChange={handleInputChange} className="form-control rgt__form-control" name="id_pais_nacimiento" id="inputGroupSelect01">
-                          {paises.map((pais) => {
-                            return (
-                              <option key={pais.id_pais} value={pais.id_pais}>
-                                {pais.nombre_pais}
-                              </option>
-                            );
-                          })}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="input-group mb-3">
-                        <p className="w-100 mb-0"> Pais de Residencia</p>
-                        <label className="input-group-text" htmlFor="inputGroupSelect02">
-                          <img src={paisResi.url_foto_pais} className="img__pais register" alt="" />
-                        </label>
-                        <select value={usuarioR.id_pais_residencia} onChange={handleInputChange} className="form-control rgt__form-control" name="id_pais_residencia" id="inputGroupSelect02">
-                          {paises.map((pais) => {
-                            return (
-                              <option key={pais.id_pais} value={pais.id_pais}>
-                                {pais.nombre_pais}
-                              </option>
-                            );
-                          })}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label">Teléfono</label>
-                        <input value={usuarioR.telefono} onChange={handleInputChange} className="form-control rgt__form-control" type="text" name="telefono" placeholder="Telefono" />
-                        <p className="text-danger fw-light d-none mt-2" ref={parrafoTelephone} style={{ fontSize: "0.75rem" }}>
-                          El telefono solo debe tener 9 a 14 numeros
-                        </p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label">RUT / DNI</label>
-                        <input value={usuarioR.rut} onChange={handleInputChange} className="form-control rgt__form-control" type="text" name="rut" placeholder="RUT o DNI" />
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label">Contraseña</label>
-                        <input value={usuarioR.password} onChange={handleInputChange} ref={refPassword} className="form-control rgt__form-control" type="password" name="password" />
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="form-label"> Confirmar contraseña </label>
-                        <input value={usuarioR.verifyPassword} onChange={handleInputChange} ref={refPasswordVerify} className="form-control rgt__form-control" type="password" name="verifyPassword" />
-                      </div>
-                    </div>
-                    <div className="col-md-12">
-                      <div className="rgt__button pb-4 p-lg-0">
-                        <button type="submit" className="btn btn__more" style={{ padding: "0.7rem 2rem" }}>
-                          Registrar
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </form>
+        </div>
+        <form className="mx-auto mb-3">
+          <div className="row align-items-end mb-lg-5 mb-md-4 mb-4">
+            <div className="col-md-6 mb-lg-0 mb-md-0 mb-4">
+              <input type="text" className="input-register" name="fullname" placeholder="Nombres y apellidos" />
+            </div>
+            <div className="col-md-6">
+              <input type="email" className="input-register" name="email" placeholder="Correo electronico" />
+            </div>
+          </div>
+          <div className="row align-items-end mb-lg-5 mb-md-4 mb-4">
+            <div className="col-md-6 mb-lg-0 mb-md-0 mb-4">
+              <div className="row">
+                <span className="ps-2 dateLabel">Fecha de nacimiento</span>
+                <div className="col-lg-2 col-md-3 col-3">
+                  <input type="number" className="input-register" name="day" placeholder="Día" />
+                </div>
+                <div className="col-lg-2 col-md-3 col-3">
+                  <input type="number" className="input-register" name="month" placeholder="Mes" />
+                </div>
+                <div className="col-lg-2 col-md-3 col-3">
+                  <input type="number" className="input-register" name="year" placeholder="Año" />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 mb-lg-0 mb-md-0 mb-4">
+              <div className="row">
+                <div className="col-lg-6 col-md-12 mb-lg-0 mb-md-0 mb-4">
+                  <input type="password" className="input-register" name="password" placeholder="Contraseña" />
+                </div>
+                <div className="col-lg-6 col-md-12">
+                  <input type="password" className="input-register" name="verifyPassword" placeholder="Confirmar contraseña" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          <label className="m-0 d-inline-flex align-items-center dateLabel fs-6">
+            <input type="checkbox" className="me-2" name="confirmTerms" />
+            Aceptar terminos y condiciones
+          </label>
+          <div className="d-grid col-lg-6 col-md-8 mx-auto mb-lg-0 mb-md-0 mb-5" style={{ marginTop: "4rem" }}>
+            <button className="btn btn_register mx-auto" type="button">Registrate</button>
+          </div>
+        </form>
       </div>
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default Register;
+export default Register
+
